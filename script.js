52017e0290a304c2752be48bed2a120f945428e7
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-11 18:21:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function fibonacci(num) {
+function fibonacci(n) {
 // your code here
+	if(n==0)return 0;
+	if(n==1)return 1;
+	var fib = fibonacci(n-1)+fibonacci(n-2);
+	return fib;
 }
 
 module.exports = fibonacci;
